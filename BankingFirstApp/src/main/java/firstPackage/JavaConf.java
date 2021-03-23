a217790662bc96a1c23265a4b6f6592f896e69d3
my pc <my pc@DESKTOP-2QD5KIO>
2021-03-23 10:42:51 +0530

    Banking project

---
+++ BankingFirstApp/src/main/java/firstPackage/JavaConf.java
@@ -0,0 +1,18 @@
+package firstPackage;
+
+public class JavaConf {
+	public static void main(String[] args) {
+
+	int i = 1;
+	/*while(i<101) {
+		System.out.println(i);
+		i++;
+	}*/
+   do {
+	   System.out.println(i);
+	   i++;
+	  
+   }
+   while(i<=50);
+}
+}
