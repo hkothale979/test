fe83b2ff8e62024704f3910a96c1788cac24b117
my pc <my pc@DESKTOP-2QD5KIO>
2021-03-23 10:45:59 +0530

    banking pro

--- BankingFirstApp/src/main/java/firstPackage/JavaConf.java
+++ BankingFirstApp/src/main/java/firstPackage/JavaConf.java
@@ -3,16 +3,6 @@ package firstPackage;
 public class JavaConf {
 	public static void main(String[] args) {
 
-	int i = 1;
-	/*while(i<101) {
-		System.out.println(i);
-		i++;
-	}*/
-   do {
-	   System.out.println(i);
-	   i++;
-	  
-   }
-   while(i<=50);
+
 }
 }
